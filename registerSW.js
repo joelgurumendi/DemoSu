a1832def1fb5aeeb8d9a2e9a654b980c9fab18e4
joelgurumendi <ljgurumendi@outlook.com>
2023-08-15 09:49:36 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/DemoSu/sw.js', { scope: '/DemoSu/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
